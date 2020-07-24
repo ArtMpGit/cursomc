a08e40b96f7bf75e51e595fb5014dcaf4c571826
Arthur Porto <arthur.porto@ctis.com.br>
2020-07-24 00:17:52 -0300

    Criação e teste da primeira classe de domínio da aplicação.

--- src/main/java/com/arthurmachado/cursomc/controllers/CategoryController.java
+++ src/main/java/com/arthurmachado/cursomc/controllers/CategoryController.java
@@ -1,15 +1,29 @@
 package com.arthurmachado.cursomc.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.arthurmachado.cursomc.domains.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryController {
 	
 	@GetMapping(value="")
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		
+		ArrayList<Category> categoryList = new ArrayList<>();
+		
+		categoryList.add(cat1);
+		categoryList.add(cat2);
+		
+		return categoryList;
 	}
 }
